c6a018124f05ea54f7053d2ee35d4a0ef1efca4b
Sevin Alipour <125585558+Alipour1179@users.noreply.github.com>
2023-02-23 22:36:46 +0330

    Add files via upload

---
+++ s2.py
@@ -0,0 +1,33 @@
+#برنامه ای بنویسید که عبارت<"hello world "> رابنویسد
+#print("hello world")
+#برنامه ای بنویسید که نوع داده های زیر را مشخس کند
+#score=0, sevin
+#score=0
+#name="sevin"
+#print(type(score),type(name))
+#حاصل جمع و تفریق وضرب و تقسیم
+# a=5,b=7اگر 
+#a=7
+#b=5
+#print("a+b=",a+b)
+#print("a-b=",a-b)
+#print("a*b=",a*b)
+# print("a/b=",a/b)
+#یک باغچه داریم به طول120متر وعرض57متربرنامه ای بنویسیدکه مساحت ومحیط باغچه را حساب کند
+#a=120
+#b=57
+#print("area=",a*b)
+#print("environment",(a+b)*2)
+#score=0
+#name="sevin"
+#print(type(score),type(name))
+#print("Your new score is", "1000 + 1")
+#print("""he said 
+# i want to go school""")
+# برنامه ای بنویسید که اندازه طول وعرض مستطیل را بگیرد و محیط و مساحت آنرا محاسبه کند
+x=int (input('enter a number1='))
+y=int (input('enter a number2='))
+envir=(x+y)*2
+area=x*y
+print("envir=",envir)
+print("area=",area)
