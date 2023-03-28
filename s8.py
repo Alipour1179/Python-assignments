53d48f6dc63f4ba4af787bdb4cd06ac64bf7e0c8
Sevin Alipour <125585558+Alipour1179@users.noreply.github.com>
2023-03-28 16:06:43 +0330

    Add files via upload

---
+++ s8.py
@@ -0,0 +1,23 @@
+# برنامه ای بنویس که نام، نام خانوادگی و ورزش مورد علاقه دانش آموز را از ورودی بگیرد و
+# پیغامب مشابه زیر پرینت نماید:
+# sara sabouri likes tennis.
+#name=input("what's your name?")
+#family=input("what's your family?")
+#sport=input("your favorite sport?")
+#print(' '+name+' '+family+' likes '+sport)
+
+#  برنامه ای بنویس که نام سه دوست را از ورودی بگیرد و با روش های گفته شده
+# یعنی با کمک
+#  f
+# و با کمک
+# +
+# وبا کمک
+# %s
+# پیغام زیر را پرینت نماید
+# sara and tina and roze are friends.
+name=input("what's your name?")
+friend1=input("what's your friend1 name?")
+friend2=input("what's your friend2 name?")
+#print(f"{name} and {friend1} and {friend2} are friends")
+#print(' '+str(name) +' and '+ str(friend1) +' and '+str(friend2) + ' '+"are friends")
+print(" %s and %s and %s are friends"%(name,friend1,friend2))
