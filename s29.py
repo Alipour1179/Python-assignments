04dcae3bd1162c0bce1d695e1da12f65bfee3467
Sevin Alipour <125585558+Alipour1179@users.noreply.github.com>
2023-08-02 18:55:36 +0330

    Add files via upload

---
+++ s29.py
@@ -0,0 +1,100 @@
+#سوال1
+# x = [1,2,3,4,5,9]
+# print(x[0:1])
+# print(x[5:6])
+# slice_x1=[]
+# for i in range(3):
+#     slice_x1.append(x[i])
+
+# print(slice_x1)
+
+# slice_x2=[]
+
+# for i in range(3,6):
+#     slice_x2.append(x[i])
+
+# print(slice_x2)
+
+# total = 0
+# for num in x:
+#     total += num
+# print(total)
+
+# length = len(x)
+# print(length)
+
+# for item in x:
+#  print(item)
+
+#سوال2
+# num1 = int(input("enter a number to add to the list:"))
+
+# x.append(num1)
+
+# num2 = int(input("enter a number to remove from the list:"))
+
+# if num2 in x:
+#     x.remove(num2)
+#     print(f"The number {num2} has been removed from the list.")
+# else:
+#     print(f"The number {num2} is not in the list.")
+
+# print(x)
+
+# print("The new list is")
+# for i in x:
+#     print(i)
+
+#سوال4
+
+
+
+
+
+
+
+#سوال4
+
+#a
+# import turtle
+
+# window = turtle.Screen()
+# window.bgcolor("white")
+# pen_turtle = turtle.Turtle()
+# pen_turtle.shape("turtle")
+# pen_turtle.color("red")
+# pen_turtle.pensize("3")
+# pen_turtle.speed(10)
+# pen_turtle.circle(50)
+# turtle.done
+
+#b
+import turtle
+
+s = turtle.Screen()
+pen = turtle.Pen()
+pen.shape('turtle')
+pen.pensize(3)
+pen.pencolor("green")
+
+for j in range(3):
+    pen.forward(200)
+    pen.left(100)
+
+turtle.done()
+# #c
+# import turtle
+
+# s = turtle.Screen()
+# pen = turtle.Pen()
+# pen.shape('turtle')
+# pen.pensize(3)
+# pen.pencolor("blue")
+
+# for i in range(3):
+#     pen.forward(200)
+#     pen.left(120)
+
+# turtle.done()
+
+#d
